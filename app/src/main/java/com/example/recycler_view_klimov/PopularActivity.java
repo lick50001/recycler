793e4a95d28b88bca5b3f0a370ffdf7e435b76ac
STEPS0N <stepan.klimoff@mail.ru>
2026-04-23 20:25:30 +0500

    Программирование окна PopularActivity. Казаков

--- app/src/main/java/com/example/recycler_view_klimov/PopularActivity.java
+++ app/src/main/java/com/example/recycler_view_klimov/PopularActivity.java
@@ -1,24 +1,87 @@
 package com.example.recycler_view_klimov;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class PopularActivity extends AppCompatActivity {
+
+    RecyclerView CategoryList, CardList;
+    TextView TvNamePage;
+    CategoryAdapter CategoryAdapter;
+    ArrayList<Category> Categorys = new ArrayList<>();
+    ArrayList<Item> Items = new ArrayList<>();
+    public Context Context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_popular);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Context = this;
+        Categorys = CategoryContext.All();
+
+        Bundle arguments = getIntent().getExtras();
+        Integer IdCategory = Integer.valueOf(arguments.get("Category").toString());
+
+        CategoryList = findViewById(R.id.category_list);
+        CardList = findViewById(R.id.card_list);
+        TvNamePage = findViewById(R.id.tv_name_page);
+
+        if (IdCategory != -1) {
+            Category SelectCategory = Categorys.stream()
+                    .filter(item -> item.Id == IdCategory)
+                    .findAny()
+                    .orElse(null);
+            SelectCategory.Active = true;
+            TvNamePage.setText(SelectCategory.Name);
+
+            CategoryAdapter = new CategoryAdapter(this, Categorys, CategoryClick);
+            CategoryList.setAdapter(CategoryAdapter);
+        } else {
+            CategoryList.setVisibility(View.GONE);
+
+            TextView TvNameCategory = findViewById(R.id.tv_name_category);
+            TvNameCategory.setVisibility(View.GONE);
+        }
+        Items = IdCategory == -1 ? ItemContext.All() : ItemContext.GetByCategory(IdCategory);
+        CardList.setLayoutManager(new GridLayoutManager(this, 2));
+        ItemAdapter CardAdapter = new ItemAdapter(this, Items);
+        CardList.setAdapter(CardAdapter);
+    }
+
+    iOnClickInterface CategoryClick = new iOnClickInterface() {
+        @Override
+        public void setClick(View view, int position) {
+            for (Category Item : Categorys)
+                Item.Active = false;
+
+            Category SelectCategory = Categorys.get(position);
+            SelectCategory.Active = true;
+            CategoryList.setAdapter(CategoryAdapter);
+
+            TvNamePage.setText(SelectCategory.Name);
+
+            Items = ItemContext.GetByCategory(SelectCategory.Id);
+            ItemAdapter CardAdapter = new ItemAdapter(Context, Items);
+            CardList.setAdapter(CardAdapter);
+        }
+    };
+
+    public void ClosePopularActivity(View view) {
+        finish();
     }
 }
