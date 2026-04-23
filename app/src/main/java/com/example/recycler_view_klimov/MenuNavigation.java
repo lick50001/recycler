e6ef8df860c48f31e4017bff9c8b0332e2cd829b
STEPS0N <stepan.klimoff@mail.ru>
2026-04-23 18:04:26 +0500

    Разработанный фрагмент навигационного меню. Казаков

---
+++ app/src/main/java/com/example/recycler_view_klimov/MenuNavigation.java
@@ -0,0 +1,19 @@
+package com.example.recycler_view_klimov;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MenuNavigation extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu_navigation, container, false);
+    }
+}
