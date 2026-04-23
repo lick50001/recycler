9ec77e2b203f94c2616046ac3899dc3123b868a1
STEPS0N <stepan.klimoff@mail.ru>
2026-04-23 20:08:03 +0500

    Программирование окна MainActivity. Казаков

--- app/src/main/java/com/example/recycler_view_klimov/MainActivity.java
+++ app/src/main/java/com/example/recycler_view_klimov/MainActivity.java
@@ -1,24 +1,62 @@
 package com.example.recycler_view_klimov;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Context Context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Context = this;
+
+        ArrayList<Category> Categorys = CategoryContext.All();
+        ArrayList<Item> Items = ItemContext.All();
+
+        RecyclerView CategoryList = findViewById(R.id.category_list);
+        RecyclerView CardList = findViewById(R.id.card_list);
+
+        CategoryAdapter CategoryAdapter = new CategoryAdapter(this, Categorys, Click);
+        CategoryList.setAdapter(CategoryAdapter);
+
+        ItemAdapter CardAdapter = new ItemAdapter(this, Items);
+        CardList.setAdapter(CardAdapter);
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        MenuNavigation fragment = new MenuNavigation();
+        ft.add(R.id.menu_navigation, fragment);
+        ft.commit();
     }
+
+    public void OpenPopularView(View view) {
+        Intent newIntent = new Intent(this, PopularActivity.class);
+        newIntent.putExtra("Category", -1);
+        startActivity(newIntent);
+    }
+
+    iOnClickInterface Click = new iOnClickInterface() {
+        @Override
+        public void setClick(View view, int position) {
+            Intent newIntent = new Intent(Context, PopularActivity.class);
+            newIntent.putExtra("Category", position);
+            startActivity(newIntent);
+        }
+    };
 }
